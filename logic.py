eb1ea7f076506be16d5a929d18a840089dc77e7d
jasiex01 <hernasj@wp.pl>
2023-01-18 15:44:52 +0100

    Initial commit with database

---
+++ logic.py
@@ -0,0 +1,17 @@
+import requests
+import json
+
+def post_json(route, json_data):
+    headers = {'Content-type': 'application/json'}
+    try:
+        response = requests.post(route, data=json.dumps(json_data), headers=headers)
+        if response.status_code != 201:
+            raise ValueError(response.text)
+    except requests.exceptions.RequestException as e:
+        print(e)
+
+def add_user(typ_uzytkownika, login, haslo, email, imienazwisko, saldo):
+    json_data = json.dumps({"TypUzytkownika": typ_uzytkownika,"Login": login,"Haslo": haslo,"Email": email,"ImieNazwisko": imienazwisko,"Saldo": saldo })
+    post_json('http://localhost:5000/insert_user', json_data)
+
+#TODO add to other tables
